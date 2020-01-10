e3006954ebc7af53b4c9941424cbc95aad5ce2f3
isoft-sistemas <adm@isoft.com.br>
2020-01-10 13:45:49 -0300

    _

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-use std::io::Read;
-use std::io::Result;
-use std::io::Write;
-use std::net::TcpStream;
 use std::collections::HashMap;
 
 //-- ----------------------
 //-- Protocol HTTP 1.1   --
-//-- Protocolo HTTP 1.1  --
 //-- ----------------------
 
 #[derive(Debug, Clone)]
@@ -75,115 +70,6 @@ impl Http{
   	
   }
   
-}
-
-
-#[derive(Debug)]
-pub struct CouchDB {
-    host: String,
-    db: String,
-    pub connected : bool,
-    pub error : bool,
-    pub msg : String,
-} 
-
-
-impl CouchDB {
-
-    // Initialize the struct
-	pub fn connect( _host : &str, _db : &str ) -> CouchDB {
-	
-	    CouchDB{
-	             host : _host.to_string(),
-		         db : _db.to_string(),
-		         connected : false,
-		         error : false,
-		         msg : String::new(),
-	    }
-	}
-}
-
-
-fn main() {
-
-    if let Err(err) = connect() {
-        println!("err = {}", err);
-    }
-
-}
-
-fn connect() -> Result<()> {
-
-    let conn = CouchDB::connect("127.0.0.1:5984", "lumiari");
-
-    let mut stream = TcpStream::connect("127.0.0.1:5984")?;
-
-    let mut http = Http::new();
-    
-    http.head.insert( "GET /lumiari HTTP/1.1".to_string(), " ".to_string());
-    http.head.insert( "Host".to_string(), "127.0.0.1:1509".to_string());
-    http.head.insert( "Connection".to_string(), "close".to_string()); 
-    http.head.insert( "Content-Type".to_string(), "application/json; charset=utf-8".to_string());
-    http.head.insert( "Content-Length".to_string(),"0".to_string());
-
-/*    
-    let mut data = String::new();
-    
-    data.push_str(
-    r#"{"_id": "07598",
-                      "id": 0,                              
-	                  "inclusao" : "2019-09-20T13:23:00Z",
-	                  "nome" : "Almir Lima",
-	                  "cpf" : "029.595.629-17",
-	                  "rg" : "7882470-0",
-	                  "dtnasc" : "26/07/1981",
-	                  "ender":{ "cep" : "85.907-000",
-	                            "lograd" : "Rua Bruno Dante Palma",
-	                            "numero" : 1535,
-	                            "bairro" : "Santa Clara 4",
-	                            "cidade" : "Toledo",
-	                            "uf" : "PR",
-	                            "fone1" : "(45) 9 9960-0519",
-	                            "fone2" : "(45) 3277-5307",
-	                            "fone3" : ""
-	                           }, 
-	                  "res_ficha" : 0,
-	                  "res_nome" : "",
-	                  "res_cpf" : "",
-	                  "res_rg" : "",
-	                  "res_dtnasc" : "",
-	                  "ind_nome" : "",
-	                  "ind_ficha" : 0,
-	                  "obs" : "Primeira consulta",
-	                  "status" : "A",
-	                  "historico" : ["|01/08/2019|Primeira Consulta","|17/09/2019|Segunda Consulta"]
-                   }"#
-    );
-    
-    
-  
-    let result = format!( "{}",  &data.len() );
-    
-    println!("Tamanho {}", result );
-
-    request_data.push_str( &data );
-
-    let request_data = request_data.replace( "$$", &result );
-    
-    
-    println!("request_data = {:?}", request_data );
-
-    let request = stream.write_all(request_data.as_bytes())?;
-  //  println!("request = {:?}", request);
-
-    let mut buf = String::new();
-    let result = stream.read_to_string(&mut buf)?;
-   
-    println!("result = {}", result);
-    println!("buf = {}", buf);
-
- */
-    Ok(())
 }
 
 /*
